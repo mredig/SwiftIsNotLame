c216b334efa37de56ba5ffa9e5bc9cd2b7d54a6f
Michael Redig <mredig@gmail.com>
2021-02-16 01:42:45 -0600

    (feat) made sinewave

--- Sources/SwiftIsNotLameExe/main.swift
+++ Sources/SwiftIsNotLameExe/main.swift
@@ -20,21 +20,21 @@ lame_set_errorf(lameGlobal) { format, args in
 	logFromLame(format, args, source: "Error")
 }
 
-lame_set_debugf(lameGlobal) { format, args in
-	logFromLame(format, args, source: "Debug")
-}
+//lame_set_debugf(lameGlobal) { format, args in
+//	logFromLame(format, args, source: "Debug")
+//}
 
 lame_set_msgf(lameGlobal) { format, args in
 	logFromLame(format, args, source: "Message")
 }
 
-lame_set_num_samples(lameGlobal, 130661)
+//lame_set_num_samples(lameGlobal, 130661)
 lame_set_num_channels(lameGlobal, 2)
 lame_set_in_samplerate(lameGlobal, 44100)
-lame_set_brate(lameGlobal, 128)
-lame_set_mode(lameGlobal, JOINT_STEREO)
-lame_set_quality(lameGlobal, 2)
-lame_set_bWriteVbrTag(lameGlobal, 0)
+lame_set_brate(lameGlobal, 256)
+lame_set_mode(lameGlobal, STEREO)
+lame_set_quality(lameGlobal, 0)
+lame_set_bWriteVbrTag(lameGlobal, 1)
 
 var success = lame_init_params(lameGlobal)
 guard success >= 0 else {
@@ -45,15 +45,36 @@ guard success >= 0 else {
 
 var myRNG = MyRNG(seed: 40)
 
-let sampleSize = 50000
+let sampleSize = 44100
 
-let fakePCM: [UInt8] = (0..<(sampleSize * 2)).map { _ in UInt8.random(in: 0..<(.max), using: &myRNG)}
-//let buffer = UnsafeBufferPointer(
-let buffer: UnsafePointer<Int16>? = fakePCM.withUnsafeBytes { bytes in
-	let address = bytes.baseAddress?.bindMemory(to: Int16.self, capacity: 50)
+let sinHz = CGFloat.random(in: 220..<880)
 
-	return UnsafePointer<Int16>(address)
+let facePCM1: [Int16] = (0..<sampleSize).map { x in
+//	Int16(sin(Double($0) * 0.00001) * Double(Int16.max))
+	let normalizePeriodValue = CGFloat.pi * 2
+	let sampleRate: CGFloat = 44100
+//	let sinHz: CGFloat = 320
+
+	let freq = sinHz * normalizePeriodValue
+
+	let x: CGFloat = CGFloat(x) / sampleRate
+
+	let y = sin(x * freq)
+	let y2 = y * CGFloat(Int16.max)
+	return Int16(y2)
 }
+
+let buffer1: UnsafePointer<Int16>? = facePCM1.withUnsafeBytes {
+	$0.baseAddress?.bindMemory(to: Int16.self, capacity: sampleSize)
+}
+
+//let fakePCM: [UInt8] = (0..<(sampleSize * 2)).map { _ in UInt8.random(in: 0..<(.max), using: &myRNG)}
+////let buffer = UnsafeBufferPointer(
+//let buffer: UnsafePointer<Int16>? = fakePCM.withUnsafeBytes { bytes in
+//	let address = bytes.baseAddress?.bindMemory(to: Int16.self, capacity: sampleSize / 2)
+//
+//	return UnsafePointer<Int16>(address)
+//}
 
 
 //var mp3Data = Data()
@@ -73,37 +94,48 @@ func printData<Element>(_ t: UnsafePointer<Element>, count: Int) {
 	print("done\n\n")
 }
 
-printData(fakePCM, count: 20)
+//printData(fakePCM, count: 20)
 
-for _ in 0..<20 {
-	let fakePCM: [UInt8] = (0..<(sampleSize * 2)).map { _ in UInt8.random(in: 0..<(.max), using: &myRNG)}
-	//let buffer = UnsafeBufferPointer(
-	let buffer: UnsafePointer<Int16>? = fakePCM.withUnsafeBytes { bytes in
-		let address = bytes.baseAddress?.bindMemory(to: Int16.self, capacity: 50)
+var mp3Out = Data()
 
-		return UnsafePointer<Int16>(address)
-	}
-
-	let bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, Int32(sampleSize), mp3Data, Int32(mp3BufferSize))
-	print("wrote \(bytesWritten) bytes")
+func appendBuffer(_ buffer: UnsafePointer<UInt8>, count: Int, to data: inout Data) {
+	let mp3Buffer = Data(bytes: buffer, count: count)
+	data.append(mp3Buffer)
 }
 
-var bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, Int32(sampleSize), mp3Data, Int32(mp3BufferSize))
+for _ in 0..<20 {
+//	let fakePCM: [UInt8] = (0..<(sampleSize * 2)).map { _ in UInt8.random(in: 0..<(.max), using: &myRNG)}
+	//let buffer = UnsafeBufferPointer(
+//	let buffer: UnsafePointer<Int16>? = fakePCM.withUnsafeBytes { bytes in
+//		let address = bytes.baseAddress?.bindMemory(to: Int16.self, capacity: 50)
+//
+//		return UnsafePointer<Int16>(address)
+//	}
+
+	let bytesWritten = lame_encode_buffer(lameGlobal, buffer1, buffer1, Int32(sampleSize), mp3Data, Int32(mp3BufferSize))
+	print("wrote \(bytesWritten) bytes")
+
+//	let mp3Buffer = Data(bytes: mp3Data, count: Int(bytesWritten))
+//	mp3Out.append(mp3Buffer)
+	appendBuffer(mp3Data, count: Int(bytesWritten), to: &mp3Out)
+}
+
+//var bytesWritten = lame_encode_buffer(lameGlobal, buffer1, buffer, Int32(sampleSize), mp3Data, Int32(mp3BufferSize))
 //bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, 50, mp3Data, Int32(mp3BufferSize))
 //bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, 50, mp3Data, Int32(mp3BufferSize))
 //bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, 50, mp3Data, Int32(mp3BufferSize))
 //bytesWritten = lame_encode_buffer(lameGlobal, buffer, buffer, 50, mp3Data, Int32(mp3BufferSize))
 
-bytesWritten = lame_encode_flush(lameGlobal, mp3Data, Int32(mp3BufferSize))
+let bytesWritten = lame_encode_flush(lameGlobal, mp3Data, Int32(mp3BufferSize))
+appendBuffer(mp3Data, count: Int(bytesWritten), to: &mp3Out)
 
 //lame_mp3_tags_fid(lameGlobal, <#T##fid: UnsafeMutablePointer<FILE>!##UnsafeMutablePointer<FILE>!#>)
 
-printData(mp3Data, count: 20)
+//printData(mp3Data, count: 20)
 
-let data = Data(bytesNoCopy: mp3Data, count: mp3BufferSize, deallocator: .free)
+//let data = Data(bytesNoCopy: mp3Data, count: mp3BufferSize, deallocator: .free)
 
 print(bytesWritten)
 
-print(data)
 
-try data.write(to: URL(fileURLWithPath: "/Users/mredig/Swap/lamed.mp3"))
+try mp3Out.write(to: URL(fileURLWithPath: "/Users/mredig/Swap/lamed.mp3"))
