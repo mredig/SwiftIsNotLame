a3459129cfb26c6e3fa5828564b9ef5f04a1eb98
Michael Redig <mredig@gmail.com>
2021-02-21 18:15:24 -0600

    (feat) successful encode from wav
    modularize main.swift with argument parser

--- Sources/SwiftIsNotLameExe/main.swift
+++ Sources/SwiftIsNotLameExe/main.swift
@@ -1,51 +1,75 @@
 import lame
 import Foundation
 import SwiftIsNotLame
+import ArgumentParser
 
-let notLame = SwiftIsNotLame()
-notLame.prepareForEncoding()
+//let sampleSize = 44100
+//func generateTone(hz: CGFloat) -> [Int16] {
+//
+//	let sinHz = hz
+//
+//	return (0..<sampleSize).map { x in
+//	//	Int16(sin(Double($0) * 0.00001) * Double(Int16.max))
+//		let normalizePeriodValue = CGFloat.pi * 2
+//		let sampleRate: CGFloat = 44100
+//	//	let sinHz: CGFloat = 320
+//
+//		let freq = sinHz * normalizePeriodValue
+//
+//		let xVal: CGFloat = CGFloat(x) / sampleRate
+//
+//		let y = sin(xVal * freq)
+//		let y2 = y * CGFloat(Int16.max)
+//		guard x != (sampleSize - 1) else { return Int16.max }
+//		return Int16(y2)
+//	}
+//}
 
-let sampleSize = 44100
-func generateTone(hz: CGFloat) -> [Int16] {
+struct SwiftIsNotLameExe: ParsableCommand {
+	@Option(name: .shortAndLong, help: "The input wav file", transform: { URL(fileURLWithPath: $0) })
+	var inputFile: URL
 
-	let sinHz = hz
+	@Option(name: .shortAndLong, help: "The output mp3 file", transform: { URL(fileURLWithPath: $0) })
+	var outputFile: URL
 
-	return (0..<sampleSize).map { x in
-	//	Int16(sin(Double($0) * 0.00001) * Double(Int16.max))
-		let normalizePeriodValue = CGFloat.pi * 2
-		let sampleRate: CGFloat = 44100
-	//	let sinHz: CGFloat = 320
 
-		let freq = sinHz * normalizePeriodValue
+	func run() throws {
+		var stopwatch = Stopwatch()
 
-		let xVal: CGFloat = CGFloat(x) / sampleRate
+		stopwatch.start("loading wav file")
+		let lampshadeWavFile = inputFile
+		let lampshadeData = try Data(contentsOf: lampshadeWavFile)
+		let testWav = WavFile(sourceData: lampshadeData)
+		stopwatch.logCheckpoint(note: "decoding wav header")
+		try testWav.decode()
 
-		let y = sin(xVal * freq)
-		let y2 = y * CGFloat(Int16.max)
-		guard x != (sampleSize - 1) else { return Int16.max }
-		return Int16(y2)
+		stopwatch.logCheckpoint(note: "getting decoded Int16 array")
+		let leftChannel: [Int16] = Array(try testWav.channelBuffer(channel: 0))
+		let rightChannel: [Int16] = Array(try testWav.channelBuffer(channel: 1))
+
+		stopwatch.logCheckpoint(note: "setting up lame")
+		let notLame = SwiftIsNotLame()
+		notLame.channels = try .init(from: testWav.channels!)
+		notLame.sampleRate = try .init(from: testWav.samplesPerSecond!)
+		notLame.mode = .stereo
+		notLame.bitRate = .CBR(rate: 256)
+		notLame.prepareForEncoding()
+		notLame.quality = 0
+
+		let leftBuffer = leftChannel.withUnsafeBufferPointer { $0 }
+		let rightBuffer = rightChannel.withUnsafeBufferPointer { $0 }
+
+		stopwatch.logCheckpoint(note: "starting mp3 encode")
+		var mp3Data = try notLame.encodeAudio(leftBuffer, rightBuffer)
+
+		mp3Data += try notLame.finishEncoding()
+
+		try mp3Data.write(to: outputFile)
+
+		stopwatch.logCheckpoint(note: "done")
+		stopwatch.printResults()
 	}
 }
 
 
-let lampLeftRaw = try Data(contentsOf: URL(fileURLWithPath: "/Users/mredig/Swap/lampshades left.raw"))
-let lampRightRaw = try Data(contentsOf: URL(fileURLWithPath: "/Users/mredig/Swap/lampshades right.raw"))
-
-let c1 = lampLeftRaw.withUnsafeBytes {
-	$0.bindMemory(to: Int16.self)
-}
-
-let c2 = lampRightRaw.withUnsafeBytes {
-	$0.bindMemory(to: Int16.self)
-}
-
-
-var mp3Data = try notLame.encodeAudio(c1, c2)
-
-let mp3Finisher = try notLame.finishEncoding()
-
-mp3Data += mp3Finisher
-
-try mp3Data.write(to: URL(fileURLWithPath: "/Users/mredig/Swap/not lame.mp3"))
-
-print("output finished")
+SwiftIsNotLameExe.main()
