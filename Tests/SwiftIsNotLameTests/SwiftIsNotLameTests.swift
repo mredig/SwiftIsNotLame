f780ba031c7c09519fd387c83e66d61716e396c3
Michael Redig <mredig@gmail.com>
2021-03-05 23:48:17 -0600

    (feat) added signed32 test

--- Tests/SwiftIsNotLameTests/SwiftIsNotLameTests.swift
+++ Tests/SwiftIsNotLameTests/SwiftIsNotLameTests.swift
@@ -5,29 +5,36 @@ import CryptoKit
 final class SwiftIsNotLameTests: XCTestCase {
 
 	func testSigned16() throws {
-		var stopwatch = Stopwatch()
-
-		stopwatch.start("loading wav file")
 		let signed16WavFile = Bundle.module.url(forResource: "signed16", withExtension: "wav", subdirectory: "TestResources")!
 
-		stopwatch.logCheckpoint(note: "decoding wav header")
 		let testWav = try WavFile(filePath: signed16WavFile)
 		try testWav.loadIntoMemory()
 		
-		let mp3Data = try convert(testWav, stopwatch: &stopwatch)
+		let mp3Data = try convert(testWav)
 
 		let hash = dataMd5Hash(mp3Data)
 
 		XCTAssertEqual("f89306722b2f30a63f27c4ad7d56a3e8", hash)
 	}
 
-	private func convert(_ wavFile: WavFile, stopwatch: inout Stopwatch) throws -> Data {
-		stopwatch.logCheckpoint(note: "setting up lame")
+	func testSigned32() throws {
+		let wavFileURL = Bundle.module.url(forResource: "signed32", withExtension: "wav", subdirectory: "TestResources")!
+
+		let testWav = try WavFile(filePath: wavFileURL)
+		try testWav.loadIntoMemory()
+
+		let mp3Data = try convert(testWav)
+
+		let hash = dataMd5Hash(mp3Data)
+
+		XCTAssertEqual("1d8d0622fba87a813d6a8de00454ae09", hash)
+	}
+
+	private func convert(_ wavFile: WavFile) throws -> Data {
 		let notLame = SwiftIsNotLame()
 
 		notLame.bitRate = .CBR(rate: 256)
 		notLame.quality = 0
-		stopwatch.logCheckpoint(note: "encoding")
 		return try notLame.encodeAudio(from: wavFile)
 	}
 
