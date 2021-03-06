6af23f3c3e7d325411efab84d96980954d0c0112
Michael Redig <mredig@gmail.com>
2021-03-05 23:45:05 -0600

    (feat) added first test

--- Tests/SwiftIsNotLameTests/SwiftIsNotLameTests.swift
+++ Tests/SwiftIsNotLameTests/SwiftIsNotLameTests.swift
@@ -1,15 +1,40 @@
 import XCTest
-//@testable import SwiftIsNotLame
+@testable import SwiftIsNotLame
+import CryptoKit
 
 final class SwiftIsNotLameTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(SwiftIsNotLame().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+	func testSigned16() throws {
+		var stopwatch = Stopwatch()
+
+		stopwatch.start("loading wav file")
+		let signed16WavFile = Bundle.module.url(forResource: "signed16", withExtension: "wav", subdirectory: "TestResources")!
+
+		stopwatch.logCheckpoint(note: "decoding wav header")
+		let testWav = try WavFile(filePath: signed16WavFile)
+		try testWav.loadIntoMemory()
+		
+		let mp3Data = try convert(testWav, stopwatch: &stopwatch)
+
+		let hash = dataMd5Hash(mp3Data)
+
+		XCTAssertEqual("f89306722b2f30a63f27c4ad7d56a3e8", hash)
+	}
+
+	private func convert(_ wavFile: WavFile, stopwatch: inout Stopwatch) throws -> Data {
+		stopwatch.logCheckpoint(note: "setting up lame")
+		let notLame = SwiftIsNotLame()
+
+		notLame.bitRate = .CBR(rate: 256)
+		notLame.quality = 0
+		stopwatch.logCheckpoint(note: "encoding")
+		return try notLame.encodeAudio(from: wavFile)
+	}
+
+	private func dataMd5Hash<D: DataProtocol>(_ data: D) -> String {
+		var hasher = Insecure.MD5()
+		hasher.update(data: data)
+		let hash = hasher.finalize()
+		return hash.map { String(format: "%02hhx", $0) }.joined()
+	}
 }
